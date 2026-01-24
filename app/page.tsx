c8342f24f2455b3bcbb2983431bf8e2679f02b01
unknown <jp.naga85@gmail.com>
2026-01-24 18:17:59 +0530

    message

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello woeld
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-green-600">
+        Tailwind v4 working ✅
+      </h1>
     </div>
   );
 }
