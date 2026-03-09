49788d776ea579d8d65c923b2846b74b2e2397b7
Jayapriya0803 <jp.naga85@gmail.com>
2026-03-09 16:13:53 +0530

    Revise metadata for improved SEO
    Updated metadata for SEO, including title and description.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,10 +3,10 @@ import Navbar from "../components/navbar";
 import Providers from "./providers";
 import CartDrawer from "@/components/cartDrawer";
 import LoginPopup from "@/components/loginPopup";
-  export const metadata = {
+export const metadata = {
   title: "Cotton Knitted Gloves Manufacturer & PPE Products Supplier | P Square Enterprises India",
   description:
-    "P Square Enterprises is a leading manufacturer of cotton knitted gloves and supplier of PPE safety products in India for industrial protection and workplace safety.",
+    "P Square Enterprises is a leading manufacturer of cotton knitted gloves and supplier of PPE safety products in India.",
   keywords: [
     "cotton knitted gloves manufacturer",
     "industrial safety gloves supplier",
@@ -14,6 +14,23 @@ import LoginPopup from "@/components/loginPopup";
     "cotton gloves manufacturer",
     "work safety gloves India"
   ],
+
+  openGraph: {
+    title: "P Square Enterprises",
+    description:
+      "Cotton knitted gloves manufacturer and PPE safety products supplier in India.",
+    url: "https://www.psquareenterprises.co.in",
+    siteName: "P Square Enterprises",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "P Square Enterprises Industrial Safety Gloves",
+      },
+    ],
+    type: "website",
+  },
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
