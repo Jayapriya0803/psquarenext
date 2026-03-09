1b57711ceb3472ca0cdf6aaeb3795f1cf7926a07
Jayapriya0803 <jp.naga85@gmail.com>
2026-03-09 15:53:08 +0530

    Add metadata for SEO in layout.tsx
    Added metadata for page title, description, and keywords.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,17 @@ import Navbar from "../components/navbar";
 import Providers from "./providers";
 import CartDrawer from "@/components/cartDrawer";
 import LoginPopup from "@/components/loginPopup";
-
+export const metadata = {
+  title: "P Square Enterprises | Cotton Knitted Gloves Manufacturer in India",
+  description:
+    "P Square Enterprises manufactures cotton knitted gloves and PPE safety products for industrial protection in India.",
+  keywords: [
+    "cotton knitted gloves",
+    "industrial safety gloves",
+    "PPE products",
+    "gloves manufacturer India"
+  ],
+};
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
